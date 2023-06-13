3b197b76bafa1410a8fd20405dcce9dc614ea6ff
Taz <pawelberowski@gmail.com>
2023-06-13 15:32:24 +0200

    Added move-right and delete buttons functionality

--- script.js
+++ script.js
@@ -1,10 +1,41 @@
 const taskName = document.querySelector('#task-name');
 const addTaskButton = document.querySelector('#add-task-button');
 const toDo = document.querySelector('#to-do')
+const doing = document.querySelector('#doing')
+const done = document.querySelector('#done')
 
-// function addEventListenersToTask(task) {
-//
-// }
+function addEventListenersToTask(task) {
+    const clonedDeleteButton = task.querySelector('.delete-button');
+    const clonedMoveRightButton = task.querySelector('.move-right-button');
+    const clonedMoveLeftButton = task.querySelector('.move-left-button');
+
+    if (clonedDeleteButton) {
+        clonedDeleteButton.addEventListener('click', function() {
+            task.remove();
+        })
+    }
+
+    if (clonedMoveRightButton) {
+        if (toDo.contains(task)) {
+            clonedMoveRightButton.addEventListener('click', function() {
+                const clonedTask = task.cloneNode(true);
+
+                doing.append(clonedTask);
+                addEventListenersToTask(clonedTask);
+                task.remove();
+            })
+        }
+        if (doing.contains(task)) {
+            clonedMoveRightButton.addEventListener('click', function() {
+                const clonedTask = task.cloneNode(true);
+                done.append(clonedTask);
+                addEventListenersToTask(clonedTask);
+                task.remove();
+            })
+        }
+    }
+
+}
 
 if (taskName && addTaskButton) {
     addTaskButton.addEventListener('click', function(){
@@ -15,10 +46,14 @@ if (taskName && addTaskButton) {
         const moveRightButton = document.createElement('button');
         moveRightButton.classList.add('move-right-button');
 
-        toDo.append(newTask, newTaskName, deleteButton, moveRightButton);
+        newTask.append(newTaskName, deleteButton, moveRightButton);
         newTaskName.innerText = taskName.value;
         deleteButton.innerText = 'Delete';
         moveRightButton.innerText = '->';
+
+
+        toDo.append(newTask);
+        addEventListenersToTask(newTask);
         taskName.value = '';
     })
 }
