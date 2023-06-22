97ede4ef7b346ab91787f71e8fe8e41f5bdbf733
Taz <pawelberowski@gmail.com>
2023-06-22 17:13:03 +0200

    Created files for listeners functions and moved functions there

--- script.js
+++ script.js
@@ -1,59 +1,11 @@
 import './styles.css'
 import { leftButtonDirection, rightButtonDirection, addMoveButton } from "./add_move_button";
-import { removeMoveButton } from "./remove_move_button";
+import {toDo, doing, done, attachListenersToMoveButton} from "./attach_listeners_to_move_button";
+import {addEventListenersToTask} from "./add_event_listeners_to_task";
 
 const taskNameInput = document.querySelector('#task-name');
 const addTaskButton = document.querySelector('#add-task-button');
-const toDo = document.querySelector('#to-do');
-const doing = document.querySelector('#doing');
-const done = document.querySelector('#done');
 
-function attachListenersToMoveButton(task, clonedTask, buttonClass) {
-    const moveButton = task.querySelector(buttonClass);
-    if (!moveButton) {
-        return;
-    }
-    if (toDo.contains(task)) {
-        moveButton.addEventListener('click', function() {
-            addMoveButton(leftButtonDirection, clonedTask);
-            doing.append(clonedTask);
-            addEventListenersToTask(clonedTask);
-            task.remove();
-        })
-    }
-    if (doing.contains(task)) {
-        moveButton.addEventListener('click', function() {
-            removeMoveButton(clonedTask, buttonClass)
-            if (buttonClass === '.move-right-button') {
-                done.append(clonedTask);
-            } else {
-                toDo.append(clonedTask);
-            }
-            addEventListenersToTask(clonedTask);
-            task.remove();
-        })
-    }
-    if (done.contains(task)) {
-        moveButton.addEventListener('click', function() {
-            addMoveButton(rightButtonDirection, clonedTask);
-            doing.append(clonedTask);
-            addEventListenersToTask(clonedTask);
-            task.remove();
-        })
-    }
-}
-function addEventListenersToTask(task) {
-    const clonedDeleteButton = task.querySelector('.delete-button');
-    const clonedTask = task.cloneNode(true);
-
-    if (clonedDeleteButton) {
-        clonedDeleteButton.addEventListener('click', function() {
-            task.remove();
-        })
-    }
-    attachListenersToMoveButton(task, clonedTask, '.move-right-button');
-    attachListenersToMoveButton(task, clonedTask,'.move-left-button');
-}
 if (taskNameInput && addTaskButton) {
     addTaskButton.addEventListener('click', function() {
         if (!taskNameInput.value) {
